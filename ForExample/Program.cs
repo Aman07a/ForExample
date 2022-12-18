984adb83c35313e1d7e13e1cc7e385b52717aabd
Aman Ahmed <aman16.aa17@gmail.com>
2022-12-18 10:03:07 +0100

    For loop

--- ForExample/Program.cs
+++ ForExample/Program.cs
@@ -4,6 +4,27 @@
 	{
 		static void Main()
 		{
+			// 1 to 10
+			for (int i = 1; i <= 10; i++)
+			{
+				System.Console.WriteLine(i + "");
+			}
+			System.Console.WriteLine();
+
+			// 0 to 9
+			for (int i = 0; i < 10; i++)
+			{
+				System.Console.WriteLine(i + "");
+			}
+			System.Console.WriteLine();
+
+			// 9 to 0
+			for (int j = 9; j >= 0; j--)
+			{
+				System.Console.WriteLine(j + "");
+			}
+
+			System.Console.ReadKey();
 		}
 	}
 }
